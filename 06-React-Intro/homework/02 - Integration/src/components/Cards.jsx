144813b6f23eeb33619901d0e5ecdb0c2a8abc60
Nazareno Carlesso <nazarenocarlessob@gmail.com>
2022-12-14 12:30:38 -0300

    review 6

--- 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -3,6 +3,9 @@ import Card from './Card';
 export default function Cards(props) {
    const { characters } = props;
    return <div>
-      {characters.map(character => (<Card props={character}></Card>))}
+      {characters.map(character => {
+         const { name, species, genre, image, onClose } = character
+         return (<Card name={name} species={species} genre={genre} image={image} onClose={onClose}></Card>)
+      })}
    </div>;
 }
--- rick_and_morty/src/components/Cards.jsx
+++
@@ -1,8 +0,0 @@
-import Card from './Card';
-
-export default function Cards(props) {
-   const { characters } = props;
-   return <div>
-      {characters.map(character => (<Card props={character}></Card>))}
-   </div>;
-}
