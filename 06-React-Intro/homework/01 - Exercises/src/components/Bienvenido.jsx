30dd852468e9792d73b604665d1d9d702dfe856f
Nazareno Carlesso <nazarenocarlessob@gmail.com>
2022-12-13 22:03:07 -0300

    exercise 6

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import Botones from "./Botones";
 
-const studentName = "";
+const studentName = "Nazareno Carlesso";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
-  // el código de tu componente acá
-  return;
+  // el código de tu componente aca
+  return (
+    <div>
+      <h1>soyHenry!</h1>
+      <h3>{studentName}</h3>
+      <ul>
+        {techSkills.map(element => (<li>{element}</li>))}
+      </ul>
+      <Botones alerts={alerts} />
+    </div>
+  );
 }
 
 // Esto lo exportamos para los tests
