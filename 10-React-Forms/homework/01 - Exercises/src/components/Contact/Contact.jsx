6483bc94366199a7960bf10eea3129f12f9051f9
Nazareno Carlesso <nazarenocarlessob@gmail.com>
2022-12-19 11:59:11 -0300

    exercise 10

--- 10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
+++ 10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
@@ -5,5 +5,85 @@ import './Contact.modules.css'
 const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 
 export default function Contact () {
-  return <div>Crear Formulario</div>
+  const [inputs, setInputs] = React.useState({
+    name: '',
+    email: '',
+    phone: 0,
+    subject: '',
+    message: ''
+  })
+  const [errors, setErrors] = React.useState(validate(inputs))
+
+  const handleChange = (event) => {
+    const key = event.target.name
+    const value = event.target.value
+    setInputs({
+      ...inputs,
+      [key]: value
+    })
+    setErrors(
+      validate({
+         ...inputs,
+         [key]: value,
+      })
+   )
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const flag = Object.values(errors).join('').length
+    console.log(inputs)
+    console.log(errors)
+    console.log(flag)
+    if (flag) return window.alert('Debe llenar todos los campos')
+    window.alert('Datos completos')
+    setInputs({
+      name: '',
+      email: '',
+      phone: 0,
+      subject: '',
+      message: ''
+    })
+    setErrors({})
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} >
+        <label>Nombre:</label>
+        <input className={errors.name && 'warning'} type='text' name='name' value={inputs.name} onChange={handleChange} id='' placeholder='Escribe tu nombre...'/>
+        <p className='danger'>{errors.name}</p>
+        <br/>
+        <label>Correo Electrónico:</label>
+        <input className={errors.email && 'warning'} type='text' name='email' value={inputs.email} onChange={handleChange} id='' placeholder='Escribe tu email...'/>
+        <p className='danger'>{errors.email}</p>
+        <br/>
+        <label>Teléfono:</label>
+        <input className={errors.phone && 'warning'} type='number' name='phone' value={inputs.phone} onChange={handleChange} id='' placeholder='Escribe un teléfono...'/>
+        <p className='danger'>{errors.phone}</p>
+        <br/>
+        <label>Asunto:</label>
+        <input className={errors.subject && 'warning'} type='text' name='subject' value={inputs.subject} onChange={handleChange} id='' placeholder='Escribe el asunto...'/>
+        <p className='danger'>{errors.subject}</p>
+        <br/>
+        <label>Mensaje:</label>
+        <textarea className={errors.message && 'warning'} type='text' name='message' value={inputs.message} onChange={handleChange} id='' placeholder='Escribe tu mensaje...'/>
+        <p className='danger'>{errors.message}</p>
+        <br/>
+        <button type='submit'>Enviar</button>
+      </form>
+    </div>
+  )
+}
+
+export const validate = (input) => {
+  const errors = {}
+
+  if (!input.name) errors.name = 'Se requiere un nombre'
+  if (!regexEmail.test(input.email)) errors.email = 'Debe ser un correo electrónico'
+  if (input.phone <= 0) errors.phone = 'Sólo números positivos'
+  if (!input.subject) errors.subject = 'Se requiere un asunto'
+  if (!input.message) errors.message = 'Se requiere un mensaje'
+  
+  return errors
 }
